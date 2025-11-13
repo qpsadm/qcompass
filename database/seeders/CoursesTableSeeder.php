7fc9c146c8de23ce115545e082df82076dac17b1
luckyneko31206-star <luckyneko31206@gmail.com>
2025-11-13 15:07:03 +0900

    Seederを編集
    ユーザーと部署を追加
    
    弘

--- database/seeders/CoursesTableSeeder.php
+++ database/seeders/CoursesTableSeeder.php
@@ -29,6 +29,43 @@ class CoursesTableSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+            [
+                'course_code' => 'C03',
+                'course_type_ID' => 3,
+                'Level_id' => 1,
+                'course_name' => 'Webプログラマー',
+                'status' => 'published',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'course_code' => 'C02',
+                'course_type_ID' => 2,
+                'Level_id' => 1,
+                'course_name' => 'OA基礎',
+                'status' => 'published',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'course_code' => 'C04',
+                'course_type_ID' => 4,
+                'Level_id' => 1,
+                'course_name' => 'Webデザイン',
+                'status' => 'published',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+
+            [
+                'course_code' => 'C101',
+                'course_type_ID' => 101,
+                'Level_id' => 1,
+                'course_name' => 'システム管理者',
+                'status' => 'published',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
         ]);
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -22,13 +22,63 @@ class UsersTableSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
-            [
+            [ //生徒さん
                 'code' => 'student01',
                 'name' => 'テスト学生',
                 'email' => 'student@example.com',
                 'password' => Hash::make('password'),
                 'courses_id' => 2, // PHP基礎
                 'role_id' => 2,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [ //事務局さん
+                'code' => 'officer01',
+                'name' => 'テスト事務局',
+                'email' => 'officer@example.com',
+                'password' => Hash::make('Password'),
+                'courses_id' => 3, //
+                'role_id' => 3,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [ //管理者さん
+                'code' => 'admin01',
+                'name' => 'テスト管理者',
+                'email' => 'admin01@example.com',
+                'password' => Hash::make('Administer'),
+                'courses_id' => 101, // 管理者
+                'role_id' => 101,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [ //非常勤さん
+                'code' => 'parttime01',
+                'name' => 'テスト非常勤',
+                'email' => 'parttime@part-example.com',
+                'password' => Hash::make('PassWord'),
+                'courses_id' => 4, // PHP基礎
+                'role_id' => 4,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'code' => 'yaruoyaruo01',
+                'name' => 'テストやる夫',
+                'email' => 'boom.yaruo@example.com',
+                'password' => Hash::make('PassWord'),
+                'courses_id' => 2, // PHP基礎
+                'role_id' => 2,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'code' => 'yiankutku01',
+                'name' => 'テスト クック先生',
+                'email' => 'kutku.sensei@example.com',
+                'password' => Hash::make('password'),
+                'courses_id' => 1, // PHP基礎
+                'role_id' => 1,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
