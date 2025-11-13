b22d4d22d1c0bc8d4c3caf9a74eed958bd2babb3
weishangli089-ux <weishangli089@gmail.com>
2025-11-13 13:10:01 +0900

    修正した
    修正した
    
    尾上

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Laravel\Scout\Searchable;
+//use Laravel\Scout\Searchable;
 
 class Course extends Model
 {
