67f9ef9823644cac5e7cd04712acedf831ce9878
weishangli089-ux <weishangli089@gmail.com>
2025-11-13 12:31:45 +0900

    モデル追加しました
    モデル追加しました
    
    尾上

---
+++ app/Models/Course.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
+
+class Course extends Model
+{
+    use HasFactory, SoftDeletes, Searchable;
+
+    protected $fillable = ['course_code', 'course_type_ID', 'Level_id', 'organizer_id', 'course_name', 'venue', 'application_date', 'certification_date', 'certification_number', 'start_date', 'end_date', 'total_hours', 'periods', 'start_time', 'finish_time', 'start_viewing', 'finish_viewing', 'plan_path', 'flier_path', 'capacity', 'entering', 'completed', 'description', 'status', 'created_user_id', 'updated_user_id', 'deleted_at', 'deleted_user_id'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'course_user', 'course_id', 'user_id');
+    }
+}
---
+++ app/Models/User copy.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Scout\Searchable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, Searchable;
+
+    public function toSearchableArray()
+    {
+        return [
+            'id' => $this->id,
+            'code' => $this->code,
+            'name' => $this->name,
+            'furigana' => $this->furigana,
+        ];
+    }
+
+    protected $fillable = [
+        'code',
+        'name',
+        'furigana',
+        'roman_name',
+        'password',
+        'role_id',
+        'courses_id',
+        'remember_token',
+        'email',
+        'email_verified_at',
+        'created_user_id',
+        'updated_user_id',
+        'deleted_at',
+        'deleted_user_id',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function courses()
+    {
+        return $this->belongsToMany(Course::class, 'course_user', 'user_id', 'course_id');
+    }
+}
