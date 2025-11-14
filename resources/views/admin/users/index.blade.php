beb9749d94c720655cdfe0e6b854f945108365cc
luckyneko31206-star <luckyneko31206@gmail.com>
2025-11-14 15:12:50 +0900

    ユーザ一覧
    ユーザー一覧に枠付けました

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container mx-auto p-4 min-h-screen" x-data="{ open: false, deleteUrl: '', deleteName: '' }">
+    <div class="container mx-auto p-4 min-h-screen bg-white rounded-lg shadow-md" x-data="{ open: false, deleteUrl: '', deleteName: '' }">
         <h1 class="text-2xl font-bold mb-4">ユーザー一覧</h1>
 
         <a href="{{ route('admin.users.create') }}"
@@ -16,6 +16,7 @@
                 検索
             </button>
         </form>
+
         <div class="overflow-x-auto">
             <table class="table-auto border-collapse border w-full text-sm">
                 <thead class="bg-gray-100">
