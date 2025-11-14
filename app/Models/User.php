5d8b86779bfbaf999dba0bdc50dd119a75a3c658
weishangli089-ux <weishangli089@gmail.com>
2025-11-14 16:24:00 +0900

    更新しました
    更新しました
    
    尾上

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,7 +50,7 @@ class User extends Authenticatable
 
     public function courses()
     {
-        return $this->belongsToMany(Course::class, 'course_user', 'user_id', 'course_id');
+        return $this->belongsToMany(Course::class, 'course_users', 'user_id', 'course_id');
     }
 
     public function role()
