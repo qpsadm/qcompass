9fbefa8168c68f04999e43a0c23065576abb2d10
weishangli089-ux <weishangli089@gmail.com>
2025-11-17 09:21:14 +0900

    修正しました
    修正しました
    
    尾上

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,12 @@
 import './bootstrap';
 import './init/alpine';
 import { initUI } from './init/ui';
-import { initCKEditor } from './init/ckeditor';
 
 import { initAgendaPreview } from './module/previewWindow';
 import { initSidebar } from './module/sidebar';
 
 document.addEventListener('DOMContentLoaded', () => {
     initUI();
-    initCKEditor();
     initAgendaPreview();
     initSidebar();
 });
