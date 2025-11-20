4566cabad9bbf0fe09bd3ca6c06c7c9a3f74b468
fukutiger <fukuchen62@gmail.com>
2025-11-20 18:41:18 +0900

    一部のSeeserを作成した
    一部のSeeserを作成した
    まだ途中のものです。
    
    福島

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,12 +15,30 @@ class UsersTableSeeder extends Seeder
 
         DB::table('users')->insert([
             [
-                'code' => 'U001',
-                'name' => '山田 太郎',
-                'furigana' => 'ヤマダ タロウ',
-                'roman_name' => 'Yamada Taro',
-                'password' => bcrypt('password123'),
+                'code' => 'webadmin',
+                'name' => 'システム管理者',
+                'furigana' => 'システムカンリシャ',
+                'roman_name' => 'Qlip Admin',
+                'password' => bcrypt('Qlip123!'),
                 'role_id' => 8,          // 管理者
+                'division_id' => 1,      // 所属部署ID
+                'courses_id' => 1,       // 存在する course の ID
+                'remember_token' => Str::random(10),
+                'email' => 'webmaster@qlipinternational.co.jp',
+                'email_verified_at' => now(),
+                'is_show' => true,
+                'created_at' => now(),
+                'created_user_name' => 'システム管理者',
+                'updated_at' => now(),
+                'updated_user_name' => 'システム管理者',
+            ],
+            [
+                'code' => 'qlip-officer',
+                'name' => 'QLIP事務員',
+                'furigana' => 'クリップ ジムイン',
+                'roman_name' => 'Qlip Teacher',
+                'password' => bcrypt('Qlip123!'),
+                'role_id' => 7,          // 管理者
                 'division_id' => 1,      // 所属部署ID
                 'courses_id' => 1,       // 存在する course の ID
                 'remember_token' => Str::random(10),
