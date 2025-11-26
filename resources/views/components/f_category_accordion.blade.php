32cbcc11ce6094c1da77b079a3760067b408ea99
shoutaroukunren6111211-wq <shoutarou.kunren.6111211@gmail.com>
2025-11-26 11:08:11 +0900

    bladeファイルを作成
    アコーディオンメニュー
    コンテンツリスト
    ページネーション
    前へ、一覧、次へボタン
    パンくずリスト
    のbladeファイルを作成。

---
+++ resources/views/components/f_category_accordion.blade.php
@@ -0,0 +1,35 @@
+<div class="accordion-menu">
+    <div class="menu-title">
+        <div class="title">
+            <span>カテゴリ</span>
+        </div>
+        <div class="accordion-btn">
+            <span></span>
+        </div>
+    </div>
+    <div class="menu-content">
+        <ul>
+            <li><a href="">電話対応スキル</a></li>
+            <li><a href="">パソコン基礎演習</a></li>
+            <li><a href="">情報技術概論</a></li>
+            <li><a href="">Javaプログラミング</a></li>
+            <li><a href="">HTML&CSSの基本</a></li>
+            <li><a href="">プロトタイプデザイン演習</a></li>
+            <li><a href="">HTML&CSSの演習</a></li>
+            <li><a href="">WEBコンテンツ制作</a></li>
+            <li><a href="">JavaScriptプログラミングの演習</a></li>
+            <li><a href="">WEBページ制作実習</a></li>
+            <li><a href="">データベース演習</a></li>
+            <li><a href="">PHPアプリ制作演習</a></li>
+            <li><a href="">LaravelによるWebアプリ制作</a></li>
+            <li><a href="">Webマーケティング概論</a></li>
+            <li><a href="">システム開発の品質管理</a></li>
+            <li><a href="">WEBアプリの企画・設計</a></li>
+            <li><a href="">WEBアプリの制作実習</a></li>
+            <li><a href="">制作発表プレゼンテーション</a></li>
+            <li><a href="">面接対応</a></li>
+            <li><a href="">就職支援</a></li>
+            <li><a href="">職業人講話</a></li>
+        </ul>
+    </div>
+</div>
---
+++ resources/views/components/f_pagination.blade.php
@@ -0,0 +1,11 @@
+<nav class="pagination">
+    <ul>
+        <li class="prev"><a href="#"></a></li>
+        <li><a href="#">1</a></li>
+        <li class="active"><span>2</span></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+        <li class="next"><a href="#"></a></li>
+    </ul>
+</nav>
