83d488815069fc897b349a5e9a358313156246b7
weishangli089-ux <weishangli089@gmail.com>
2025-11-26 21:48:32 +0900

    更新
    更新
    
    尾上

--- resources/views/components/f_content_list.blade.php
+++ resources/views/components/f_content_list.blade.php
@@ -1,8 +1,8 @@
 <div class="content-list">
     <table>
         @foreach ($items as $item)
+        @if ($item->status===2) {{-- status が true の場合のみ --}}
         @php
-        // 講座名の決定
         $courseName = $item->course?->name ?: '本講座';
         if (empty($courseName)) {
         $courseName = '全講座';
@@ -10,26 +10,18 @@
         @endphp
 
         <tr>
-            {{-- 日付 --}}
-            <td class="date">
-                {{ $item->created_at->format('Y/m/d') }}
-            </td>
-
-            {{-- カテゴリー --}}
+            <td class="date">{{ $item->created_at->format('Y/m/d') }}</td>
             <td class="category">
-                <p class="category-{{ $item->type?->slug ?? 'default' }}">
-                    {{ $courseName }}
-                </p>
+                <p class="category-{{ $item->type?->slug ?? 'default' }}">{{ $courseName }}</p>
             </td>
-
-            {{-- タイトル（講座名 + カテゴリー名 + タイトル） --}}
             <td class="title">
                 <a href="{{ route('user.announcements.show', $item->id) }}">
-                    【{{ $item->type?->type_name ?? '未分類' }}】
-                    {{ $item->title }}
+                    【{{ $item->type?->type_name ?? '未分類' }}】{{ $item->title }}
                 </a>
             </td>
         </tr>
+        @endif
         @endforeach
+
     </table>
 </div>
