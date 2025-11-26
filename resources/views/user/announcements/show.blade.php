9a799846c570dbe904548e392ad53e5ba5aa6059
chas-pooh <chas.qlip@gmail.com>
2025-11-26 21:54:52 +0900

    Update show.blade.php
    bladeファイルを更新しました。
    
    矢野

--- resources/views/user/announcements/show.blade.php
+++ resources/views/user/announcements/show.blade.php
@@ -1,8 +1,21 @@
 @extends('layouts.f_layout')
 
+@section('code-page-css')
+    <link rel="stylesheet" href="{{ asset('assets/css/f_editor.css') }}">
+@endsection
+
 @section('main-content')
-<h1>{{ $announcement->title }}</h1>
-<p>講座: {{ $announcement->course?->name ?? '全講座' }}</p>
-<p>カテゴリー: {{ $announcement->type?->name ?? '未分類' }}</p>
-<div>{!! $announcement->content !!}</div>
+    <div class="container">
+        <x-f_page_title :search="true" title="{{ $announcement->title }}" />
+        {{-- <h1>{{ $announcement->title }}</h1> --}}
+        {{-- <p>講座: {{ $announcement->course?->name ?? '全講座' }}</p>
+        <p>カテゴリー: {{ $announcement->type?->name ?? '未分類' }}</p> --}}
+        <div class="page-content">
+            <div>{!! $announcement->content !!}</div>
+        </div>
+
+        <x-f_btn_list :prevBtn="true" :listBtn="true" :nextBtn="true" />
+
+        <x-f_bread_crumbs />
+    </div>
 @endsection
