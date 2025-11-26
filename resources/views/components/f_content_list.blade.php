0718879442c6f21e528013574324aeed0b017003
weishangli089-ux <weishangli089@gmail.com>
2025-11-26 15:45:08 +0900

    更新
    更新
    
    おのえ

--- resources/views/components/f_content_list.blade.php
+++ resources/views/components/f_content_list.blade.php
@@ -1,60 +1,31 @@
-{{-- <div class="content-list">
-    <table>
-        <tr>
-            <td class="date">2025/10/29</td>
-            <td class="category">
-                <p class="category-news">WEBシステム</p>
-            </td>
-            <td class="title"><a href="">【お知らせ】明日（11/20）休講のお知らせ</a></td>
-        </tr>
-        <tr>
-            <td class="date">2025/10/29</td>
-            <td class="category">
-                <p class="category-agenda">全体</p>
-            </td>
-            <td class="title"><a href="">【イベント】講座体験イベントのご案内</a></td>
-        </tr>
-        <tr>
-            <td class="date">2025/10/28</td>
-            <td class="category">
-                <p class="category-agenda">全体</p>
-            </td>
-            <td class="title"><a href="">【就職支援】就職支援講座のご案内</a></td>
-        </tr>
-        <tr>
-            <td class="date">2025/10/24</td>
-            <td class="category">
-                <p class="category-agenda">全体</p>
-            </td>
-            <td class="title"><a href="">【お知らせ】施設内環境の保全について</a></td>
-        </tr>
-        <tr>
-            <td class="date">2025/10/22</td>
-            <td class="category">
-                <p class="category-news">WEBシステム</p>
-            </td>
-            <td class="title"><a href="news_info.html">【キャリコン】第2回キャリアコンサルティングについて</a></td>
-        </tr>
-    </table>
-</div> --}}
-@props(['items'])
-
 <div class="content-list">
     <table>
         @foreach ($items as $item)
+            @php
+                // 講座名の決定
+                $courseName = $item->course?->name ?: '本講座';
+                if (empty($courseName)) {
+                    $courseName = '全講座';
+                }
+            @endphp
+
             <tr>
+                {{-- 日付 --}}
                 <td class="date">
                     {{ $item->created_at->format('Y/m/d') }}
                 </td>
 
+                {{-- カテゴリー --}}
                 <td class="category">
                     <p class="category-{{ $item->type?->slug ?? 'default' }}">
-                        {{ $item->type?->name ?? '未分類' }}
+                        {{ $courseName }}
                     </p>
                 </td>
 
+                {{-- タイトル（講座名 + カテゴリー名 + タイトル） --}}
                 <td class="title">
                     <a href="{{ route('user.announcements.show', $item->id) }}">
+                        [{{ $item->type?->type_name ?? '未分類' }}]
                         {{ $item->title }}
                     </a>
                 </td>
--- resources/views/user/announcements/show.blade.php
+++ resources/views/user/announcements/show.blade.php
@@ -2,5 +2,7 @@
 
 @section('content')
     <h1>{{ $announcement->title }}</h1>
-    <p>{{ $announcement->content }}</p>
+    <p>講座: {{ $announcement->course?->name ?? '全講座' }}</p>
+    <p>カテゴリー: {{ $announcement->type?->name ?? '未分類' }}</p>
+    <div>{!! $announcement->content !!}</div>
 @endsection
