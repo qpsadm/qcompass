680935b98d68f32930dafe23cfa7099e4d9312b8
chas-pooh <chas.qlip@gmail.com>
2025-11-27 14:09:03 +0900

    Update agendas_info.blade.php
    アジェンダ詳細のbladeファイルを更新しました。
    
    矢野

--- resources/views/user/agenda/agendas_info.blade.php
+++ resources/views/user/agenda/agendas_info.blade.php
@@ -6,16 +6,21 @@
 
 @section('main-content')
     <div class="container">
-        <h1 class="text-2xl font-bold mb-4">{{ $agenda->agenda_name }}</h1>
+        <x-f_page_title :search="false" title="{{ $agenda->agenda_name }}" />
 
         {{-- <p>カテゴリーID: {{ $agenda->category_id }}</p> --}}
         <div class="page-content">
-            <p>講座ID: {!! $agenda->content !!}</p>
+            {!! $agenda->content !!}
         </div>
         {{-- <p>ステータス: {{ $agenda->status }}</p>
         <p>表示フラグ: {{ $agenda->is_show }}</p> --}}
         {{-- <p>{{ $agenda->created_at->format('Y/m/d') }}</p> --}}
 
-        <a href="{{ route('user.agendas.my') }}" class="text-blue-600 hover:underline">一覧に戻る</a>
+        {{-- <a href="{{ route('user.agendas.my') }}" class="text-blue-600 hover:underline">一覧に戻る</a> --}}
+
+        <x-f_btn_list :prevBtn="true" :listBtn="true" :nextBtn="true" listUrl="{{ url('user/agendas') }}"
+            listLabel="一覧へ" />
+
+        <x-f_bread_crumbs />
     </div>
 @endsection
