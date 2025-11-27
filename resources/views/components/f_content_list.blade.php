18d4f4de34c9befda1668e45b0e9d676f60fc3ec
weishangli089-ux <weishangli089@gmail.com>
2025-11-27 13:48:06 +0900

    更新
    更新
    
    おのえ

--- resources/views/components/f_content_list.blade.php
+++ resources/views/components/f_content_list.blade.php
@@ -1,28 +1,29 @@
 <div class="content-list">
     <table>
         @foreach ($items as $item)
-        @php
-        // course_name が空でなければ "本講座" にする
-        $courseName = $item->course?->course_name;
-        if (!empty($courseName)) {
-        $courseName = '本講座';
-        } else {
-        $courseName = '全講座';
-        }
-        @endphp
-        <tr>
-            <td class="date">{{ $item->created_at->format('Y/m/d') }}</td>
-            <td class="category">
-                <p class="category-{{ $item->type?->slug ?? 'default' }} {{ !empty($item->course?->course_name) ? 'course-hon' : 'course-all' }}">
-                    {{ $courseName }}
-                </p>
-            </td>
-            <td class="title">
-                <a href="{{ route('user.news.news_info', $item->id) }}">
-                    【{{ $item->type?->type_name ?? '未分類' }}】{{ $item->title }}
-                </a>
-            </td>
-        </tr>
+            @php
+                // course_name が空でなければ "本講座" にする
+                $courseName = $item->course?->course_name;
+                if (!empty($courseName)) {
+                    $courseName = '本講座';
+                } else {
+                    $courseName = '全講座';
+                }
+            @endphp
+            <tr>
+                <td class="date">{{ $item->created_at->format('Y/m/d') }}</td>
+                <td class="category">
+                    <p
+                        class="category-{{ $item->type?->slug ?? 'default' }} {{ !empty($item->course?->course_name) ? 'course-hon' : 'course-all' }}">
+                        {{ $courseName }}
+                    </p>
+                </td>
+                <td class="title">
+                    <a href="{{ route('user.news.news_info', $item->id) }}">
+                        【{{ $item->type?->type_name ?? '未分類' }}】{{ $item->title }}
+                    </a>
+                </td>
+            </tr>
         @endforeach
     </table>
 </div>
--- resources/views/user/agenda/agendas_list.blade.php
+++ resources/views/user/agenda/agendas_list.blade.php
@@ -8,7 +8,7 @@
             <p>表示できるアジェンダはありません。</p>
         @else
             @foreach ($agendasByCategory as $categoryId => $agendas)
-                <div class="mb-6">
+                {{-- <div class="mb-6">
                     <h2 class="text-xl font-semibold mb-2">カテゴリーID: {{ $categoryId }}</h2>
                     <ul class="list-disc ml-5">
                         @foreach ($agendas as $agenda)
@@ -20,8 +20,21 @@
                             </li>
                         @endforeach
                     </ul>
-                </div>
+                </div> --}}
             @endforeach
         @endif
+    </div>
+@endsection
+@extends('layouts.f_layout')
+
+@section('main-content')
+    <div class="container">
+        <x-f_page_title :search="true" title="新着情報一覧" />
+        <x-f_category_list />
+
+        <x-f_content_list :items="$agendasByCategory" />
+
+        <x-f_pagination />
+        <x-f_bread_crumbs />
     </div>
 @endsection
