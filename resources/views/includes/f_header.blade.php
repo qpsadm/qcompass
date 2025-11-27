0779a385cabe7f5466d81fbcb6cdf0b4930076a0
chas-pooh <chas.qlip@gmail.com>
2025-11-27 12:34:07 +0900

    各種ファイルの更新
    各種ファイルを更新しました。
    
    矢野

--- resources/views/includes/f_header.blade.php
+++ resources/views/includes/f_header.blade.php
@@ -4,16 +4,16 @@
             <ul>
                 <li class="active"><a class="home" href="{{ route('user.dashboard') }}">ホーム</a></li>
                 <li><a class="news" href="{{ route('user.news.news_list') }}">お知らせ</a></li>
-                <li><a class="agenda" href="{{ route('user.agenda.agendas_list') }}">アジェンダ</a></li>
-                <li><a class="study" href="{{ route('user.questions.questions_list') }}">学習支援</a></li>
-                <li><a class="work" href="{{ route('user.job.job_offers_list') }}">就職支援</a></li>
-                <li><a class="mypage" href="{{ route('user.mypage.mypage') }}">マイページ</a></li>
+                <li><a class="agenda" href="{{-- {{ route('user.agenda.agendas_list') }} --}}">アジェンダ</a></li>
+                <li><a class="study" href="{{-- {{ route('user.questions.questions_list') }} --}}">学習支援</a></li>
+                <li><a class="work" href="{{-- {{ route('user.job.job_offers_list') }} --}}">就職支援</a></li>
+                <li><a class="mypage" href="{{-- {{ route('user.mypage.mypage') }} --}}">マイページ</a></li>
             </ul>
         </nav>
 
         <!-- responsive -->
         <div class="site-logo">
-            <a href="{{ route('user.top') }}"><img src="{{ asset('assets/images/f_site-logo.svg') }}"
+            <a href="{{-- {{ route('user.top') }} --}}"><img src="{{ asset('assets/images/f_site-logo.svg') }}"
                     alt="コンパスロゴ"></a>
         </div>
         <div class="hamburger-btn">
