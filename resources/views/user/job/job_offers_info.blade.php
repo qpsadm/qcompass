b4ab67f480cc36f2b48b4af948bc79ac7a8aa985
shoutaroukunren6111211-wq <shoutarou.kunren.6111211@gmail.com>
2025-11-27 12:12:28 +0900

    就職支援ページの更新
    就職支援ページを更新しました。
    
    増井

---
+++ resources/views/user/job/job_offers_info.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.f_layout')
+
+@section('code-page-css')
+    <link rel="stylesheet" href="{{ asset('assets/css/f_editor.css') }}">
+@endsection
+
+@section('main-content')
+    <div class="container">
+        <x-f_page_title :search="false" title="{{ $announcement->title }}" />
+
+        <div class="page-content">
+            <div>{!! $announcement->content !!}</div>
+        </div>
+
+        <x-f_btn_list :prevBtn="true" :listBtn="true" :nextBtn="true" listUrl="{{ url('user/job') }}"
+            listLabel="一覧画面へ" />
+
+        <x-f_bread_crumbs />
+    </div>
+@endsection
