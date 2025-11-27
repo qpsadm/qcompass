2dc5a20eb15eab182a7a193841270295664ed082
chas-pooh <chas.qlip@gmail.com>
2025-11-27 14:04:56 +0900

    Update agendas_list.blade.php
    アジェンダリストのbladeファイルを更新しました。
    
    矢野

--- resources/views/user/agenda/agendas_list.blade.php
+++ resources/views/user/agenda/agendas_list.blade.php
@@ -2,37 +2,26 @@
 
 @section('main-content')
     <div class="container">
-        <h1 class="text-2xl font-bold mb-4">自分の講座アジェンダ一覧</h1>
+        <x-f_page_title :search="true" title="最新のアジェンダ一覧" />
 
-        @if ($agendasByCategory->isEmpty())
-            <p>表示できるアジェンダはありません。</p>
-        @else
-            @foreach ($agendasByCategory as $categoryId => $agendas)
-                {{-- <div class="mb-6">
-                    <h2 class="text-xl font-semibold mb-2">カテゴリーID: {{ $categoryId }}</h2>
-                    <ul class="list-disc ml-5">
-                        @foreach ($agendas as $agenda)
-                            <li>
-                                <a href="{{ route('user.agenda.detail', ['id' => $agenda->id]) }}"
-                                    class="text-blue-600 hover:underline">
+        <x-f_category_accordion />
+
+        @foreach ($agendasByCategory as $categoryId => $agendas)
+            <div class="content-list">
+                <table>
+                    @foreach ($agendas as $agenda)
+                        <tr>
+                            <td class="date">{{ $agenda->created_at }}</td>
+                            <td class="title">
+                                <a href="{{ route('user.agenda.info', ['id' => $agenda->id]) }}">
                                     {{ $agenda->agenda_name }}
                                 </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div> --}}
-            @endforeach
-        @endif
-    </div>
-@endsection
-@extends('layouts.f_layout')
-
-@section('main-content')
-    <div class="container">
-        <x-f_page_title :search="true" title="新着情報一覧" />
-        <x-f_category_list />
-
-        <x-f_content_list :items="$agendasByCategory" />
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        @endforeach
 
         <x-f_pagination />
         <x-f_bread_crumbs />
