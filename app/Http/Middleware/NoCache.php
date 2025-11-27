0c24c59c85519feba97234d98c6898ffbcdd3752
luckyneko31206-star <luckyneko31206@gmail.com>
2025-11-27 13:36:20 +0900

    アジェンダとお知らせのファイル
    なおしましたー
    
    弘

--- app/Http/Middleware/NoCache.php
+++ app/Http/Middleware/NoCache.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class NoCache
 {
@@ -11,8 +12,18 @@ class NoCache
     {
         $response = $next($request);
 
-        return $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
-            ->header('Pragma', 'no-cache')
-            ->header('Expires', 'Sat, 01 Jan 1990 00:00:00 GMT');
+        // BinaryFileResponseの場合にヘッダーを追加
+        if ($response instanceof BinaryFileResponse) {
+            $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+            $response->headers->set('Pragma', 'no-cache');
+            $response->headers->set('Expires', 'Sat, 01 Jan 1990 00:00:00 GMT');
+        } else {
+            // その他のレスポンスの場合は通常の方法でヘッダーを設定
+            $response->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+                ->header('Pragma', 'no-cache')
+                ->header('Expires', 'Sat, 01 Jan 1990 00:00:00 GMT');
+        }
+
+        return $response;
     }
 }
