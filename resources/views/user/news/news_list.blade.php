8dcb0d59e922b24fbd8bf91af64b49c1200af74f
weishangli089-ux <weishangli089@gmail.com>
2025-11-28 10:26:02 +0900

    更新
    更新
    
    尾上

--- resources/views/user/news/news_list.blade.php
+++ resources/views/user/news/news_list.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.f_layout')
 
 @section('main-content')
-<div class="container">
-    <x-f_page_title :search="true" title="新着情報一覧" />
-    <x-f_category_list />
+    <div class="container">
+        <x-f_page_title :search="true" title="新着情報一覧" />
+        <x-f_category_list type="news" :category="$category ?? 'all'" />
 
-    <x-f_content_list :items="$announcements" />
+        <x-f_content_list :items="$announcements" />
 
-    <x-f_pagination />
-    <x-f_bread_crumbs />
-</div>
+        <x-f_pagination />
+        <x-f_bread_crumbs />
+    </div>
 @endsection
