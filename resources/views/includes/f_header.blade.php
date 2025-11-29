303d25fea30445a9fa403fe3b40bb5cb4ed26891
weishangli089-ux <weishangli089@gmail.com>
2025-11-29 13:47:06 +0900

    kousinn
    更新
    
    尾上

--- resources/views/includes/f_header.blade.php
+++ resources/views/includes/f_header.blade.php
@@ -6,7 +6,7 @@
                 <li class="{{ request()->routeIs('user.news.*') ? 'active' : '' }}"><a class="news" href="{{ route('user.news.news_list') }}">お知らせ</a></li>
                 <li class="{{ request()->routeIs('user.agenda.*') ? 'active' : '' }}"><a class="agenda" href="{{ route('user.agenda.agendas_list') }}">アジェンダ</a></li>
                 <li class="{{ request()->routeIs('user.question.*') ? 'active' : '' }}"><a class="study" href="{{ route('user.question.questions_list') }}">学習支援</a></li>
-                <li class="{{ request()->routeIs('user.job.*') ? 'active' : '' }}"><a class="work" href="{{-- {{ route('user.job.job_offers_list') }} --}}">就職支援</a></li>
+                <li class="{{ request()->routeIs('user.job.*') ? 'active' : '' }}"><a class="work" href="{{ route('user.job.job_offers_list') }}">就職支援</a></li>
                 <li class="{{ request()->routeIs('user.mypage.*') ? 'active' : '' }}"><a class="mypage" href="{{-- {{ route('user.mypage.mypage') }} --}}">マイページ</a></li>
             </ul>
         </nav>
