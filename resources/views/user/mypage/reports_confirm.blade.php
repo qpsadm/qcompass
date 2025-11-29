9b29cb20b7ef0d5fe9a3c162e290d2bdb72adfda
weishangli089-ux <weishangli089@gmail.com>
2025-11-29 21:30:58 +0900

    更新
    更新
    
    尾上

--- resources/views/user/mypage/reports_confirm.blade.php
+++ resources/views/user/mypage/reports_confirm.blade.php
@@ -4,8 +4,8 @@
 <div class="container mx-auto p-4 max-w-3xl">
     <h1 class="text-2xl font-bold mb-4">日報送信完了</h1>
     <p>日報の送信が完了しました。</p>
-    <a href="{{ route('user.reports_info') }}" class="mt-4 inline-block text-blue-600 hover:underline">
-        日報一覧に戻る
+    <a href="{{ route('user.top') }}" class="mt-4 inline-block text-blue-600 hover:underline">
+        TOPに戻る
     </a>
 </div>
 @endsection
