85184b09832fe08c80c89b2cb32b8feaab6d89e4
chas-pooh <chas.qlip@gmail.com>
2025-11-29 21:57:41 +0900

    ログインのエラーメッセージ更新
    ログインのエラーメッセージの表示方法を更新しました。
    
    矢野

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,36 +6,40 @@
         @csrf
 
         <div class="login-item">
-            <label for="name">会社名</label>
-            <p>株式会社 QLIP</p>
+            <div class="item-box">
+                <label for="name">会社名</label>
+                <p>株式会社 QLIP</p>
+            </div>
         </div>
 
         <!-- Course 選択 / 表示 -->
         <div class="login-item">
 
-            {{-- <x-input-label for="course_id" :value="__('Course')" /> --}}
-            <label for="name">講座名</label>
+            <div class="item-box">
+                {{-- <x-input-label for="course_id" :value="__('Course')" /> --}}
+                <label for="name">講座名</label>
 
-            @if ($selected_course)
-                {{-- URLで指定された場合はラベル表示 --}}
-                <span class="block mt-1 w-full border-gray-300 rounded-md shadow-sm bg-gray-100 p-2">
-                    {{ $courses->find($selected_course)->course_name ?? '不明なコース' }}
-                </span>
-                <input type="hidden" name="course_id" value="{{ $selected_course }}">
-            @else
-                {{-- URL指定なしならセレクトボックス --}}
-                <select id="course_id" name="course_id" required
-                    class="block mt-1 w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
-                    <option value="">選択してください</option>
-                    @foreach ($courses as $course)
-                        <option value="{{ $course->id }}" {{ old('course_id') == $course->id ? 'selected' : '' }}>
-                            {{ $course->course_name }}
-                        </option>
-                    @endforeach
-                </select>
-            @endif
+                @if ($selected_course)
+                    {{-- URLで指定された場合はラベル表示 --}}
+                    <span class="block mt-1 w-full border-gray-300 rounded-md shadow-sm bg-gray-100 p-2">
+                        {{ $courses->find($selected_course)->course_name ?? '不明なコース' }}
+                    </span>
+                    <input type="hidden" name="course_id" value="{{ $selected_course }}">
+                @else
+                    {{-- URL指定なしならセレクトボックス --}}
+                    <select id="course_id" name="course_id" required
+                        class="block mt-1 w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
+                        <option value="">選択してください</option>
+                        @foreach ($courses as $course)
+                            <option value="{{ $course->id }}" {{ old('course_id') == $course->id ? 'selected' : '' }}>
+                                {{ $course->course_name }}
+                            </option>
+                        @endforeach
+                    </select>
+                @endif
+            </div>
 
-            <x-input-error :messages="$errors->get('course_id')" class="mt-2" />
+            <x-input-error :messages="$errors->get('course_id')" class="error-msg" />
         </div>
 
 
@@ -43,18 +47,23 @@
         <!-- Name -->
         <div class="login-item">
             {{-- <x-input-label for="login_name" :value="__('Name')" /> --}}
-            <label for="name">お名前</label>
-            <x-text-input id="login_name" class="block mt-1 w-full" type="text" name="login_name" :value="old('login_name')" placeholder="yamada_taro" required autofocus />
-            <x-input-error :messages="$errors->get('login_name')" class="mt-2" />
+            <div class="item-box">
+                <label for="name">お名前</label>
+                <x-text-input id="login_name" class="block mt-1 w-full" type="text" name="login_name"
+                    :value="old('login_name')" placeholder="yamada_taro" required autofocus />
+            </div>
+            <x-input-error :messages="$errors->get('login_name')" class="error-msg" />
         </div>
 
         <!-- Password -->
         <div class="login-item">
             {{-- <x-input-label for="password" :value="__('Password')" /> --}}
-            <label for="name">パスワード</label>
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" placeholder="パスワードを入力してください" required
-                autocomplete="current-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <div class="item-box">
+                <label for="name">パスワード</label>
+                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password"
+                    placeholder="パスワードを入力してください" required autocomplete="current-password" />
+            </div>
+            <x-input-error :messages="$errors->get('password')" class="error-msg" />
         </div>
 
         <p class="considerations">※パスワード等をお忘れの場合は、管理者までお問い合わせください。</p>
