8caa2bacbc1118079d7c8f3f1428e16757b34881
weishangli089-ux <weishangli089@gmail.com>
2025-11-30 16:37:23 +0900

    更新
    更新
    
    尾上

--- resources/views/components/f_category_list.blade.php
+++ resources/views/components/f_category_list.blade.php
@@ -1,7 +1,6 @@
-@props(['type' => 'news', 'tags' => []])
+@props(['type' => 'news', 'tags' => [], 'category' => 'all'])
 
 @if($type === 'news')
-{{-- ニュースはカテゴリータブを表示 --}}
 @php
 $categories = [
 'all' => 'ALL',
@@ -9,13 +8,14 @@ $categories = [
 'my' => '本講座に関するお知らせ',
 ];
 
+// タブごとのリンク
 $routes = [
 'all' => route('user.news.news_list', ['category' => 'all']),
 'main' => route('user.news.main_news'),
 'my' => route('user.news.my_news'),
 ];
 
-$currentCategory = request('category', 'all');
+$currentCategory = $category ?? 'all'; // ここでコントローラーから渡された category を使う
 @endphp
 
 <div class="category-menu">
@@ -30,25 +30,16 @@ $currentCategory = request('category', 'all');
 @endif
 
 @if($type === 'question' && count($tags))
-{{-- 質疑応答はタグだけ表示 --}}
 <div class="category-menu">
     <ul>
-        {{-- ALL --}}
         <li class="{{ request('tag') === null ? 'active' : '' }}">
-            <a href="{{ route('user.question.questions_list') }}">
-                ALL
-            </a>
+            <a href="{{ route('user.question.questions_list') }}">ALL</a>
         </li>
-
-        {{-- タグ一覧 --}}
         @foreach($tags as $tag)
         <li class="{{ request('tag') == $tag->id ? 'active' : '' }}">
-            <a href="{{ route('user.question.questions_list', ['tag' => $tag->id]) }}">
-                {{ $tag->name }}
-            </a>
+            <a href="{{ route('user.question.questions_list', ['tag' => $tag->id]) }}">{{ $tag->name }}</a>
         </li>
         @endforeach
     </ul>
 </div>
-
 @endif
