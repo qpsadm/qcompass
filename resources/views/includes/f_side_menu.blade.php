952c72ca0ef0d6e4b6183b4233029a6956ba395b
chas-pooh <chas.qlip@gmail.com>
2025-11-30 16:25:54 +0900

    各種ファイルの更新
    各種ファイルを更新しました。
    
    矢野

--- resources/views/includes/f_side_menu.blade.php
+++ resources/views/includes/f_side_menu.blade.php
@@ -17,48 +17,47 @@
                     </div>
                 </div>
 
-                @foreach($courses as $course)
-                <div class="course-item">
-                    <div class="countdown">
-                        <p class="countdown-title">修了まであと</p>
-                        <div class="countdown-data">
-                            <span class="data-number">{{ $course->remaining_days }}</span>
-                            <span class="data-sub-title">日</span>
+                @foreach ($courses as $course)
+                    <div class="course-item">
+                        <div class="countdown">
+                            <p class="countdown-title">修了まであと</p>
+                            <div class="countdown-data">
+                                <span class="data-number">{{ $course->remaining_days }}</span>
+                                <span class="data-sub-title">日</span>
+                            </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
 
                 <div class="today-short">
                     <p class="short-title">今日のひとこと</p>
 
-                    @if(!empty($todayQuote))
-                    <div class="short-text">
-                        @if($quote_mode === 'mix' && Session::has('quote_parts'))
-                        @foreach(Session::get('quote_parts') as $part)
-                        {{ $part->text }}
-                        @endforeach
-                        @else
-                        {{ $todayQuote->quote_full }}
-                        @endif
-                    </div>
+                    @if (!empty($todayQuote))
+                        <div class="short-text">
+                            @if ($quote_mode === 'mix' && Session::has('quote_parts'))
+                                @foreach (Session::get('quote_parts') as $part)
+                                    {{ $part->text }}
+                                @endforeach
+                            @else
+                                {{ $todayQuote->quote_full }}
+                            @endif
+                        </div>
 
-                    <div class="short-name">
-                        （
-                        @if($quote_mode === 'mix' && Session::has('author_parts'))
-                        @foreach(Session::get('author_parts') as $part)
-                        {{ $part->text }}
-                        @endforeach
-                        @else
-                        {{ $todayQuote->author_full ?? '作者不明' }}
-                        @endif
-                        ）
-                        <button class="inline-toggle" data-mode="{{ $quote_mode === 'full' ? 'mix' : 'full' }}" onclick="toggleQuoteMode(event)">?</button>
-                    </div>
-
-
+                        <div class="short-name">
+                            （
+                            @if ($quote_mode === 'mix' && Session::has('author_parts'))
+                                @foreach (Session::get('author_parts') as $part)
+                                    {{ $part->text }}
+                                @endforeach
+                            @else
+                                {{ $todayQuote->author_full ?? '作者不明' }}
+                            @endif
+                            ）
+                            <button class="inline-toggle" data-mode="{{ $quote_mode === 'full' ? 'mix' : 'full' }}"
+                                onclick="toggleQuoteMode(event)">?</button>
+                        </div>
                     @else
-                    <span class="short-text">名言が登録されていません</span>
+                        <span class="short-text">名言が登録されていません</span>
                     @endif
                 </div>
 
@@ -84,14 +83,15 @@
 
         <div class="side-menu-bottom">
             <ul class="side-menu-list">
-                @foreach($courses as $course)
-                @if($course->plan_path)
-                <li>
-                    <a class="calendar-list" href="{{ asset('storage/' . $course->plan_path) }}" target="_blank">
-                        日別計画表
-                    </a>
-                </li>
-                @endif
+                @foreach ($courses as $course)
+                    @if ($course->plan_path)
+                        <li>
+                            <a class="calendar-list" href="{{ asset('storage/' . $course->plan_path) }}"
+                                target="_blank">
+                                日別計画表
+                            </a>
+                        </li>
+                    @endif
                 @endforeach
                 </li>
                 <li><a class="question" href="{{ route('user.question.questions_list') }}">質疑応答</a></li>
