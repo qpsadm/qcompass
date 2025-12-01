0cb9a5e9175842cc575cca4a023cf220356beaf6
weishangli089-ux <weishangli089@gmail.com>
2025-12-01 14:55:17 +0900

    更新
    更新
    
    おのえ

--- resources/views/layouts/b_navbar.blade.php
+++ resources/views/layouts/b_navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="fixed z-30 w-full bg-white border-b border-gray-200">
+<nav class="fixed z-30 w-full bg-blue-300 border-b border-gray-200">
     <div class="px-3 py-3 md:px-4 lg:px-5">
         <div class="flex items-center justify-between">
 
