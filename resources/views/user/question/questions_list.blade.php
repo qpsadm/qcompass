5d4d4ffbf1faac851f1b3772e02ed12382dc5d31
luckyneko31206-star <luckyneko31206@gmail.com>
2025-12-02 10:14:31 +0900

    質疑応答のページネーション
    修正
    
    弘

--- resources/views/user/question/questions_list.blade.php
+++ resources/views/user/question/questions_list.blade.php
@@ -47,57 +47,8 @@
     </div>
 
 {{-- ページネーション --}}
-@php
-    $lastPage = $questions->lastPage();
-    $currentPage = $questions->currentPage();
-    $prevPage = $currentPage > 1 ? $currentPage - 1 : null;
-    $nextPage = $currentPage < $lastPage ? $currentPage + 1 : null;
-@endphp
-
-<nav class="pagination w-full flex justify-center mt-6 mb-6">
-    <ul class="flex flex-wrap justify-center gap-2 items-center">
-
-        {{-- 前へ --}}
-        <li>
-            @if ($prevPage)
-
-                <a href="{{ $questions->url($prevPage) }}" class="px-3 py-1 rounded text-orange-500 hover:text-orange-600 text-[18px]">&#x25C0;</a>
-            @else
-
-                <span class="px-3 py-1 rounded text-orange-300 text-[18px] cursor-not-allowed">&#x25C0;</span>
-            @endif
-        </li>
-
-        {{-- ページ番号 --}}
-        @for ($i = 1; $i <= $lastPage; $i++)
-            <li>
-                @if ($i == $currentPage)
-
-                    <span class="px-3 py-1 bg-blue-500 text-white rounded">{{ $i }}</span>
-                @else
-
-                    <a href="{{ $questions->url($i) }}" class="px-3 py-1 border rounded hover:bg-gray-200 text-[18px]">{{ $i }}</a>
-                @endif
-            </li>
-        @endfor
-
-        {{-- 次へ --}}
-        <li>
-            @if ($nextPage)
-
-                <a href="{{ $questions->url($nextPage) }}" class="px-3 py-1 rounded text-orange-500 hover:text-orange-600 text-[18px]">
-                    &#x25B6;
-                </a>
-            @else
-
-                <span class="px-3 py-1 rounded text-orange-300 text-[18px] cursor-not-allowed">
-                    &#x25B6;
-                </span>
-            @endif
-        </li>
-
-    </ul>
-</nav>
+    <x-f_pagination :paginator="$questions" />
+    <x-f_bread_crumbs />
 
 </div>
 @endsection
