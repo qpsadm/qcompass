bb055028c325f26ed481e519d41105858a4953d8
weishangli089-ux <weishangli089@gmail.com>
2025-12-02 09:11:59 +0900

    更新
    更新
    
    おのえ

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -90,6 +90,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect()->route('login');
     }
 }
