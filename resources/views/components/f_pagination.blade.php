2c18329f3529b4c376a342292775bbcb4eb9100c
weishangli089-ux <weishangli089@gmail.com>
2025-12-03 13:19:02 +0900

    更新
    更新
    
    尾上

--- resources/views/components/f_pagination.blade.php
+++ resources/views/components/f_pagination.blade.php
@@ -1,33 +1,56 @@
 @props(['paginator'])
 
-<nav class="pagination">
-    <ul>
-        {{-- 前のページリンク --}}
-        <li class="prev {{ $paginator->onFirstPage() ? 'disabled' : '' }}">
-            @if ($paginator->onFirstPage())
-            <span></span>
-            @else
-            <a href="{{ $paginator->previousPageUrl() }}"></a>
+@if ($paginator->hasPages())
+    <nav class="pagination">
+        <ul>
+            {{-- 前ページリンク --}}
+            <li class="{{ $paginator->onFirstPage() ? 'disabled' : '' }}">
+                @if ($paginator->onFirstPage())
+                    <span>«</span>
+                @else
+                    <a href="{{ $paginator->previousPageUrl() }}">«</a>
+                @endif
+            </li>
+
+            {{-- ページ番号リンク（省略対応） --}}
+            @php
+                $current = $paginator->currentPage();
+                $last = $paginator->lastPage();
+                $start = max(1, $current - 2);
+                $end = min($last, $current + 2);
+            @endphp
+
+            {{-- 先頭ページ --}}
+            @if ($start > 1)
+                <li><a href="{{ $paginator->url(1) }}">1</a></li>
+                @if ($start > 2)
+                    <li class="disabled"><span>…</span></li>
+                @endif
             @endif
-        </li>
 
-        {{-- ページ番号リンク --}}
-        @foreach ($paginator->links()->elements[0] as $page => $url)
-        @if ($page == $paginator->currentPage())
-        <li class="active"><span>{{ $page }}</span></li>
-        @else
-        <li><a href="{{ $url }}">{{ $page }}</a></li>
-        @endif
-        @endforeach
+            {{-- 中央ページ --}}
+            @for ($i = $start; $i <= $end; $i++)
+                <li class="{{ $i == $current ? 'active' : '' }}">
+                    <a href="{{ $paginator->url($i) }}">{{ $i }}</a>
+                </li>
+            @endfor
 
-        {{-- 次のページリンク --}}
-        <li class="next {{ $paginator->hasMorePages() ? '' : 'disabled' }}">
-            @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}"></a>
-            @else
-            <span></span>
+            {{-- 末尾ページ --}}
+            @if ($end < $last)
+                @if ($end < $last - 1)
+                    <li class="disabled"><span>…</span></li>
+                @endif
+                <li><a href="{{ $paginator->url($last) }}">{{ $last }}</a></li>
             @endif
-        </li>
-    </ul>
-</nav>
 
+            {{-- 次ページリンク --}}
+            <li class="{{ $paginator->hasMorePages() ? '' : 'disabled' }}">
+                @if ($paginator->hasMorePages())
+                    <a href="{{ $paginator->nextPageUrl() }}">»</a>
+                @else
+                    <span>»</span>
+                @endif
+            </li>
+        </ul>
+    </nav>
+@endif
