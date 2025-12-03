e86d24296ab4b5b1fa3b19d056e41a55c496581e
chas-pooh <chas.qlip@gmail.com>
2025-12-03 11:36:21 +0900

    各種ファイルを更新
    各種ファイルを更新しました。
    
    矢野

--- resources/views/user/teacher/teachers_list.blade.php
+++ resources/views/user/teacher/teachers_list.blade.php
@@ -3,28 +3,26 @@
 @section('title', '講師一覧')
 
 @section('main-content')
-<div class="container">
+    <div class="container">
 
-    <div class="page-title">
-        <h2>講師一覧</h2>
+        <x-f_page_title :search="false" title="講師一覧" />
+
+        @if ($teachers->isEmpty())
+            <p>あなたの講座に担当の先生はいません。</p>
+        @else
+            <ul class="teacher-list">
+                @foreach ($teachers as $teacher)
+                    <li class="teacher-item">
+                        <a href="{{ route('user.teacher.teachers_info', $teacher->id) }}">
+                            <div class="teacher-name">{{ $teacher->name }}</div>
+                            <div class="teacher-division">
+                                所属：{{ $teacher->division->name ?? '未設定' }}
+                            </div>
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        @endif
+
     </div>
-
-    @if ($teachers->isEmpty())
-    <p>あなたの講座に担当の先生はいません。</p>
-    @else
-    <ul class="teacher-list">
-        @foreach ($teachers as $teacher)
-        <li class="teacher-item">
-            <a href="{{ route('user.teacher.teachers_info', $teacher->id) }}">
-                <div class="teacher-name">{{ $teacher->name }}</div>
-                <div class="teacher-division">
-                    所属：{{ $teacher->division->name ?? '未設定' }}
-                </div>
-            </a>
-        </li>
-        @endforeach
-    </ul>
-    @endif
-
-</div>
 @endsection
