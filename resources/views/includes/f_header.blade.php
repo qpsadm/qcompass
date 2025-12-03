a55176cbef5c08588aebb6f50a798f0225d89e8b
chas-pooh <chas.qlip@gmail.com>
2025-12-03 10:12:37 +0900

    Update f_header.blade.php
    ヘッダーを更新しました。
    
    矢野

--- resources/views/includes/f_header.blade.php
+++ resources/views/includes/f_header.blade.php
@@ -70,7 +70,7 @@
                             </div>
 
                             <div class="short-name">
-                                （
+                                -
                                 @if ($quote_mode === 'mix' && Session::has('author_parts'))
                                 @foreach (Session::get('author_parts') as $part)
                                 {{ $part->text }}
@@ -78,7 +78,7 @@
                                 @else
                                 {{ $todayQuote->author_full ?? '作者不明' }}
                                 @endif
-                                ）
+                                -
                                 {{-- <button class="inline-toggle" data-mode="{{ $quote_mode === 'full' ? 'mix' : 'full' }}"
                                     onclick="toggleQuoteMode(event)">?</button> --}}
                             </div>
