089174d0dbab3c2c5284c143a895754b4de25fe6
luckyneko31206-star <luckyneko31206@gmail.com>
2025-12-03 11:42:13 +0900

    修正
    更新
    
    弘

--- resources/views/admin/course_category/index.blade.php
+++ resources/views/admin/course_category/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container mx-auto p-4 min-h-screen bg-white rounded-lg shadow-md" x-data="{ open: false, deleteUrl: '', deleteName: '' }">
 
-        <h1 class="text-2xl font-bold mb-4 text-gray-800">講座カテゴリ一覧</h1>
+        <h1 class="text-2xl font-bold mb-4 text-gray-800">講座カテゴリ</h1>
 
         {{-- テーブル --}}
         <div class="overflow-x-auto">
