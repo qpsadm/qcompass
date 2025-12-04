458f1eabf51d796f752db5114d361e1cb66dc055
myu9u6-ai <myu.9u6@gmail.com>
2025-12-04 19:12:28 +0900

    ファイルを更新しました
    ファイルを更新しました　黒津

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -10,7 +10,9 @@
 @section('main-content')
     <div class="container">
         <x-f_page_title :search="false" title="本サイトについて" />
-
+        <div class="page-content">
+            <img src="{{ asset('assets/images/f_about.png') }}" alt="">
+        </div>
         <x-f_btn_list :prevBtn="false" :listBtn="true" :nextBtn="false" listUrl="{{ route('user.top') }}"
             listLabel="トップへもどる" />
 
