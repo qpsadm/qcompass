74fd617908cd4ef7c1e633da6363e865502b1a73
weishangli089-ux <weishangli089@gmail.com>
2025-12-05 21:28:12 +0900

    更新
    更新
    
    尾上

--- resources/views/components/f_btn_list.blade.php
+++ resources/views/components/f_btn_list.blade.php
@@ -1,26 +1,26 @@
 @props([
-    'prevBtn',
-    'listBtn',
-    'nextBtn',
-    'listUrl',
-    'listLabel',
-    'prevUrl' => null,
-    'nextUrl' => null
+'prevBtn',
+'listBtn',
+'nextBtn',
+'listUrl',
+'listLabel',
+'prevUrl' => null,
+'nextUrl' => null
 ])
 
 <div class="btn-list">
     <ul>
-        @if ($prevBtn)
-            <li class="short-btn"><a href="{{ $prevUrl }}">前へ</a></li>
-        @endif
-
-        @if ($listBtn)
-            <li class="default-btn"><a href="{{ $listUrl }}">{{ $listLabel }}</a></li>
-        @endif
 
         @if ($nextBtn)
-            <li class="short-btn"><a href="{{ $nextUrl }}">次へ</a></li>
+        <li class="short-btn"><a href="{{ $nextUrl }}">前へ</a></li>
         @endif
+        @if ($listBtn)
+        <li class="default-btn"><a href="{{ $listUrl }}">{{ $listLabel }}</a></li>
+        @endif
+        @if ($prevBtn)
+        <li class="short-btn"><a href="{{ $prevUrl }}">次へ</a></li>
+        @endif
+
 
     </ul>
 </div>
--- resources/views/layouts/b_footer.blade.php
+++ resources/views/layouts/b_footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="fixed text-center bottom-0 left-0 w-full bg-blue-300 text-white p-4">
+<footer class="fixed text-center bottom-0 left-0 w-full bg-blue-300 text-gray-600 p-4">
     <p>Copyright© QLIPプログラミングスクール All Rights Reserved.</p>
 </footer>
